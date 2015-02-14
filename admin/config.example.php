3f4de352d74cb1226ccfa69ac64e0a2b54c65613
Sander Ploegsma <sanderploegsma@gmail.com>
2015-02-14 02:18:22 +0100

    Added example config file

---
+++ admin/config.example.php
@@ -0,0 +1,9 @@
+<?php
+/*
+    Define server settings here.
+*/
+define('SERVER', 'localhost');
+define('USERNAME', 'user');
+define('PASSWORD', 'password');
+define('DATABASE', 'database');
+define('TABLE', 'table');
