647e8e86eebd4e72cec366aa11dba6f401fb01ec
Sander Ploegsma <sanderploegsma@gmail.com>
2015-02-26 19:42:14 +0100

    Fix edit alert
    Displaying only the url instead of the entire anchor object makes more
    sense.

--- admin/assets/wisvch.js
+++ admin/assets/wisvch.js
@@ -102,7 +102,7 @@ $('#addItem').click(function(){
 $('body').on('click', '.edit-button', function(){
   var id = $(this).closest('tr').attr('id');
   var key = $(this).closest('td').next('td').next('td').html();
-  var url = $(this).closest('td').next('td').next('td').next('td').html();
+  var url = $(this).closest('td').next('td').next('td').next('td').find('a').text();
   var idnum = id.split("-")[1];
 
   bootbox.dialog({
